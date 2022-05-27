0dc7ec2f4a9c3806663e8ae34e64ae46f3d2b04d
Sanjana <pingali4@illinois.edu>
2022-05-27 15:31:42 -0500

    years flags merged based on how recent year is

--- src_code/rank_researchers.py
+++ src_code/rank_researchers.py
@@ -383,8 +383,8 @@ def main():
     db = mysql.connector.connect(
     host='localhost',
     user="root",
-    password="14converseS@",
-    database="database_week_4"
+    password=<password>,
+    database=<database>
     )
     
     cur = db.cursor()
