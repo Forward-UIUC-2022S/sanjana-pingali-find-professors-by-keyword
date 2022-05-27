9c2f1339b8149873667dfb013c96ff82bed583d9
Sanjana <pingali4@illinois.edu>
2022-05-27 16:52:35 -0500

    csv of predictions generated

--- src_code/rank_researchers.py
+++ src_code/rank_researchers.py
@@ -431,8 +431,8 @@ def main():
     db = mysql.connector.connect(
     host='localhost',
     user="root",
-    password="14converseS@",
-    database="database_week_4"
+    password=<pasword>,
+    database=<database>
     )
     
     cur = db.cursor()
